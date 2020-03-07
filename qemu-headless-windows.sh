ada070a84b448a97efe86db9ccd764f664c7d64d
Thesicstar <sicstar21@gmail.com>
2020-03-07 13:28:19 +0100

    + cleanup

--- qemu-headless-windows.sh
+++ qemu-headless-windows.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-#Get VirtIO drivers from https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/
-#Generate own MAC Address!
 
 WINIMG=/path/to/win10.iso
 VIRTIMG=/path/to/virtio-drivers.iso
 VMLOC=/path/to/vm/location
+
 qemu-system-x86_64 \
 -enable-kvm \
--daemonize \
 -m 8G \
 -machine q35,accel=kvm \
 -smp cores=2,threads=4 \
@@ -24,5 +22,3 @@ qemu-system-x86_64 \
 -nographic \
 -vnc :0 \
 -k en-us
-# add -net user,smb=$HOME if you want a shared drive
-# add hostfwd=protocoll::hostport-:guestport to line 23 for port forwarding to VM 
